fe85d0ce59451f5ddda1f258c0d601f34571ad38
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2024-10-31 21:09:09 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ArrowRight, Cpu, Lightbulb, Scale } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
-function Home() {
+export default function Home() {
   return (
     <div className="pt-16">
       {/* Hero Section */}
